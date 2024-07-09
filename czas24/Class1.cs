2b0e4e58c57afc4a1e6fd6f9ded99aab08ec1b19
yulia <yulia@YULLIA>
2024-07-09 14:04:07 +0200

    Add project files.

---
+++ czas24/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace czas24
+{
+    public class Czas24h
+    {
+        private int liczbaSekund;
+
+        public int Sekunda
+        {
+            get => liczbaSekund - Godzina * 60 * 60 - Minuta * 60;
+            set
+            {
+                    if (value < 0 || value >= 60)
+                        throw new ArgumentException("error");
+
+                    int cz_bez_sek = liczbaSekund - (liczbaSekund - Godzina * 60 * 60 - Minuta * 60);
+                    liczbaSekund = cz_bez_sek + value;
+            }
+        }
+
+        public int Minuta
+        {
+            get => (liczbaSekund / 60) % 60;
+            set
+            {
+                if (value <= 0 || value >= 60)
+                    throw new ArgumentException("error");
+
+                int cz_bez_min = liczbaSekund - (((liczbaSekund / 60) % 60)*60);
+                liczbaSekund = cz_bez_min + (value * 60);
+            }
+        }
+
+        public int Godzina
+        {
+            get => liczbaSekund / 3600;
+            set
+            {
+                if (value < 0 || value >= 24)
+                    throw new ArgumentException("error");
+
+                int cz_bez_god = liczbaSekund - (liczbaSekund / 3600 * 3600);
+                liczbaSekund = cz_bez_god + (value * 3600);
+            }
+        }
+
+        public Czas24h(int godzina, int minuta, int sekunda)
+        {
+            if (sekunda < 0 || sekunda >= 60 || minuta < 0 || minuta >= 60 || godzina < 0 || godzina >= 24)
+                throw new ArgumentException("error");
+
+            liczbaSekund = sekunda + 60 * minuta + 3600 * godzina;
+        }
+
+        public override string ToString() => $"{Godzina}:{Minuta:D2}:{Sekunda:D2}";
+    }
+}
---
+++ czas24/Program.cs
@@ -0,0 +1,65 @@
+﻿using czas24;
+
+
+//Test1();
+//Test2();
+//Test3();
+//Test4();
+Test5();
+
+
+
+
+
+void Test1()
+{
+    // Test 1
+    var t = new Czas24h(2, 15, 37);
+    t.Minuta = 20;
+    t.Godzina = 1;
+    t.Sekunda = 26;
+    Console.WriteLine(t);
+}
+
+void Test2()
+{
+    // Test 2
+    var t = new Czas24h(2, 15, 37);
+    t.Minuta = 20;
+    t.Godzina = 24;
+    Console.WriteLine(t);
+}
+
+void Test3()
+{
+    // Test 3
+    var t = new Czas24h(2, 15, 37);
+    t.Minuta = -20;
+    t.Godzina = 23;
+    Console.WriteLine(t);
+}
+
+void Test4()
+{
+    // Test 4
+    var t = new Czas24h(2, 15, 37);
+    t.Minuta = 20;
+    t.Godzina = 23;
+    t.Godzina = 1;
+    t.Sekunda = 15;
+    t.Minuta = 10;
+    t.Sekunda = 23;
+    t.Sekunda = 1;
+    t.Minuta = 12;
+    Console.WriteLine(t);
+}
+
+void Test5()
+{
+    // Test 5
+    var t = new Czas24h(24, 15, 37);
+    t.Minuta = 20;
+    t.Godzina = 23;
+    t.Godzina = 1;
+    Console.WriteLine(t);
+}
